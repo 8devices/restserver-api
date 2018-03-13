544475432eaec5f2fffa10da7715f28a516753dc
Giedrius <giedrius.medzevicius@gmail.com>
2018-03-13 11:39:44 +0200

    Remove changes to resourceInstance

--- 8dev_emulate/nodes/resourceInstance.js
+++ 8dev_emulate/nodes/resourceInstance.js
@@ -29,7 +29,7 @@ class Resource extends EventEmitter {
 }
 
 class ResourceInstance extends EventEmitter {
-  constructor(identifier, permissions, type, value = undefined, handler = undefined, notifyOnChange = true) {
+  constructor(identifier, permissions, type, value = undefined, handler = undefined, notifyOnChange = false) {
     super();
 
     this.identifier = identifier;
@@ -47,6 +47,7 @@ class ResourceInstance extends EventEmitter {
 
   set value(value) {
     // TODO: throw exception if value type is incorrect
+
     if (this._value != value) {
       this._value = value;
       this.emit('change', value);
